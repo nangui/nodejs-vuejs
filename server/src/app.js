c5b7d75846300215ae255204bc678cc4b2ad91e6
Adonaï Elohim Exaucé <adonainangui03@gmail.com>
2018-07-01 12:30:26 +0000

    Ajout du server et configuration

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+let bodyParser = require('body-parser')
+let cors = require('cors')
+let morgan = require('morgan')
+let app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('combined'))
+
+app.use((req, res, next) => {
+  res.send('Welcome to hour server')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server listening on port 3000')
+})
